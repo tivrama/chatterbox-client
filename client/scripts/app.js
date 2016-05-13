77e44507670ab739919dfcda57e084081e71041c
tivrama <tivrama@yahoo.com>
2016-05-13 12:30:01 -0700

    Progress!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,29 @@
 // YOUR CODE HERE:
+//https://api.parse.com/1/classes/chatterbox
 
+var app = {
+  init: function(){},
+
+  send: function(message){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+
+
+  },
+
+  fetch: function(){}
+};
+
+console.log(app.send('hello'));
