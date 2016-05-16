ac2733159c9b482440c9e69651cc77b81b9aca71
tivrama <tivrama@yahoo.com>
2016-05-15 19:50:02 -0700

    Cleaning up code with help from solutions.  Working on post

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,102 @@
 // YOUR CODE HERE:
 //https://api.parse.com/1/classes/chatterbox
-
 // &, <, >, ", ', `, , !, @, $, %, (, ), =, +, {, }, [, and ]
+//Example post: 
+//var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
 
 var app = {
+  //Object to hold all messages
   data: {},
+  //list all friends
   friends: [],
+  //save server to a more managable var 
   server: 'https://api.parse.com/1/classes/chatterbox',
-  userName: (window.location.search).match(/(&|\?)username=(.+)/)[2],
-// sort: {'createdAt': -1},
+  //save username from window object
+  userName: window.location.search.substr(10),
 
+  //loads messages with fetch at the opening of the page (original version)
+  // init: function(){
+  //   app.fetch();
+  // },
 
-  init: function(){
+  //loads messages with fetch at the opening of the page 
+  init: function() {
+    // cache a dom reference so I don't repeat messages:
+    app.$text = $('#message');
     app.fetch();
-    // console.log('test');
-    // $('#chats').text('hello');
+    //setInterval(app.fetch.bind(app),1000);
+    $('#send').on('submit', app.handleSubmit);
+  },
+
+
+
+  fetch: function() {
+    $.ajax({
+      url: app.server,
+      data: { order: '-createdAt'},
+      contentType: 'application/json',
+      success: function(json) {
+        app.displayMessages(json.results);
+      },
+      // complete: function() {
+      //   app.stopSpinner();
+      // }
+    })
+  },
+
+  //Original fetch
+  // fetch: function(){
+  //   $.ajax({
+  //     url: app.server,
+  //     type: 'GET',
+  //     contentType: 'application/json; charset=utf-8',
+  //     data: 'order=-createdAt',
+  //     success: function(data) {
+  //       app.data = data;
+
+  //       // console.log('data["results"]: ', data['results']);
+
+  //       app.sortData(data)
+  //     },
+
+  //     error: function(data) {
+  //       console.error('chatterbox: Failed to get data');
+  //     }
+  //   });
+  // },
+
+  displayMessage: function(message) {
+    if ( !app.data[message.objectId]) {
+      var $html = app.renderMessage(message);
+      $('#chats').append($html);
+      app.data[message.objectId] = true;
+    }
+  }, 
+
+  displayMessages: function(messages) {
+    for (var i = 0; i < messages.length; i++) {
+      app.displayMessage(messages[i]);
+    }
+  },
+
+  renderMessage: function(message) {
+    var $user = $("<div>",{class: 'user'}).text(message.username);
+    var $text = $("<div>", {class:'text'}).text(message.text);
+    var $message = $("<div>", {class: 'chat', 'data-id': message.objectId}).append($user, $text);
+    return $message;
+  },
+
+  handleSubmit: function() {
+    // e.preventDefault();
+    var message = {
+      username: app.username,
+      text: app.$text.val()
+    };
   },
 
   send: function(message){
@@ -25,91 +107,76 @@ var app = {
       //add linter to prevent illegal messages
       data: JSON.stringify(message),
       dataType: 'json',
-      contentType: 'application/json; charset=utf-8',
+      contentType: 'application/json',
       success: function (data) {
-        // console.log('chatterbox: Message sent. Message: ', message);
         app.clearMessages();
         app.fetch();
+        // message.objectId = json.objectId;
+        // app.displayMessage(message)
       },
+      // complete: function() {
+      //   app.stopSpinner();
+      // }
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
       }
     });
-
-
   },
 
-  fetch: function(){
-    $.ajax({
-      url: app.server,
-      type: 'GET',
-      contentType: 'application/json; charset=utf-8',
-      data: 'order=-createdAt',
-      success: function(data) {
-        app.data = data;
-        // data['results'] = data['results'].sort(function(a,b) {
-        //   return a.createdAt < b.createdAt ? -1 : 1;
-        // });
 
-        console.log('sorted data:', data['results']);
+  // sortData: function(data) {
+  //   var node = {
+  //     username: '',
+  //     date: '',
+  //     message: '',
+  //     room: '',
+  //     objectId: ''
+  //   };
 
-        app.sortData(data)
-      },
+  //   var entityMap = {
+  //     "&": "&amp;",
+  //     "<": "&lt;",
+  //     ">": "&gt;",
+  //     '"': '&quot;',
+  //     "'": '&#39;',
+  //     "/": '&#x2F;'
+  //   };
 
-      error: function(data) {
-        console.error('chatterbox: Failed to get data');
-      }
-    });
-  },
+  //   //loop through data
+  //   var chats = data.results;
+  //   for (var i = 0; i < chats.length; i++) {
+  //     for (var k in chats[i]) {
+  //       // console.log(chats[i][k])
+  //       if (k === 'objectId') {
+  //         node.room = chats[i][k];
+  //       }
+  //       else if (k === 'username') {
+  //         node.username = chats[i][k];
+  //       }
+  //       else if (k === 'createdAt') {
+  //         node.date = chats[i][k];
+  //       }
+  //       else if (k === 'roomname') {
+  //         node.room = chats[i][k];
+  //       }
+  //       else if (k === 'text') {
+  //         // node.message = JSON.stringify(chats[i][k]).replace('setInterval', '');
+  //         node.message = JSON.stringify(chats[i][k]).replace(/[&<>\/]/g, function(char) {
+  //           return entityMap[char];
+  //         });
+  //         // .escape("&<>\"'`!@$%()=+{}[]")
+  //       }
+  //       var msg = $('<div>').addClass('message');
+  //       msg.append($('<h3>').text(node.username).addClass('username'));
+  //       msg.append($('<p>').text(node.message));
+  //       msg.append($('<p>').text(node.date));
+  //       msg.append($('<p>').text(node.room));
 
-  sortData: function(data) {
-    var node = {
-      username: '',
-      date: '',
-      message: '',
-      room: ''
-    };
-
-    var entityMap = {
-      "&": "&amp;",
-      "<": "&lt;",
-      ">": "&gt;",
-      '"': '&quot;',
-      "'": '&#39;',
-      "/": '&#x2F;'
-    };
-
-    //loop through data
-    var chats = data.results;
-    for (var i = 0; i < chats.length; i++) {
-      for (var k in chats[i]) {
-        if (k === 'username') {
-          node.username = chats[i][k];
-        }
-        else if (k === 'createdAt') {
-          node.date = chats[i][k];
-        }
-        else if (k === 'roomname') {
-          node.room = chats[i][k];
-        }
-        else if (k === 'text') {
-          // node.message = JSON.stringify(chats[i][k]).replace('setInterval', '');
-          node.message = JSON.stringify(chats[i][k]).replace(/[&<>"'\/]/g, function(char) {
-            return entityMap[char];
-          });
-          // .escape("&<>\"'`!@$%()=+{}[]")
-        }
-        var msg = $('<div>').addClass('message');
-        msg.append($('<h3>').text(node.username).addClass('username'));
-        msg.append($('<p>').text(node.message));
-        msg.append($('<p>').text(node.date));
-        msg.append($('<p>').text(node.room));
-
-        $("#chats").append(msg);
-      }
-    }
-  },
+  //       app.appendMessage(msg);
+  //     }
+  //   };
+  // },
 
   clearMessages: function() {  
     $('#chats').empty();
@@ -121,9 +188,15 @@ var app = {
     msg.append($('<p>').text(message.text));
     msg.append($('<p>').text(message.roomname));
     
-    $('#chats').append(msg);
     app.send(message);
+    app.appendMessage(msg)
   },
+
+
+  appendMessage: function(message) {
+    $('#chats').append(message);
+  },
+
 
   addRoom: function(room) {
     var option = $('<option>').text(room);
@@ -136,18 +209,21 @@ var app = {
   },
 
 
-  handleSubmit: function(message) {
-    app.send(message);
-    //app.fetch();
+  // handleSubmit: function(message) {
+  //   app.send(message);
+  //   app.fetch();
 
-  },
+  // },
+
+
+
 
 
 
 };
 
 $(document).ready(function() {
-  app.init(); // Initialize app
+  // app.init(); // Initialize app
   
   $('#clear').on('click', function() {
     app.clearMessages();
@@ -155,25 +231,127 @@ $(document).ready(function() {
 
   $('#main').on('click', '.username', function() {
     app.addFriend(this.textContent);
-    console.log(app.friends);
+    // console.log(app.friends);
   });
 
   $('.submit').on('click', function(e) {
     e.preventDefault();
     var msg = $('#message').val();
     var newMsg = {
+      username: app.userName,
       text: msg,
       //createdAt: new Date(),
-      roomname: 'lobby',
-      username: app.userName
+      roomname: 'lobby'
       //updatedAt: new Date()
     };
-    console.log(newMsg);
     app.handleSubmit(newMsg);
   });
 
 
 });
+
+///////////////////////////////////////////////////////////////////////
+// Simplified jQuery-based implementation of chatterbox-client
+///////////////////////////////////////////////////////////////////////
+
+
+// var app = {
+
+//   server: 'https://api.parse.com/1/classes/chatterbox',
+
+//   init: function() {
+//     console.log('running chatterbox');
+
+//     // Get username:
+//     app.username = window.location.search.substr(10);
+
+//     app.onscreenMessages = {};
+
+//     // cache a dom reference:
+//     app.$text = $('#message');
+
+//     app.loadMsgs();
+//     setInterval(app.loadMsgs.bind(app),1000);
+
+//     $('#send').on('submit', app.handleSubmit);
+//   },
+
+//   handleSubmit: function(e) {
+//     e.preventDefault();
+//     var message = {
+//       username: app.username,
+//       text: app.$text.val()
+//     };
+
+//     app.$text.val('');
+
+//     app.sendMsg(message);
+//   },
+
+//   renderMessage: function(message) {
+//     var $user = $("<div>",{class: 'user'}).text(message.username);
+//     var $text = $("<div>", {class:'text'}).text(message.text);
+//     var $message = $("<div>", {class: 'chat', 'data-id': message.objectId}).append($user, $text);
+//     return $message;
+//   },
+
+//   displayMessage: function(message) {
+//     if( !app.onscreenMessages[message.objectId]) {
+//       var $html = app.renderMessage(message);
+//       $('#chats').prepend($html);
+//       app.onscreenMessages[message.objectId] = true;
+//     }
+//   }, 
+
+//   displayMessages: function(messages) {
+//     for(var i = 0; i < messages.length; i++) {
+//       app.displayMessage(messages[i]);
+//     }
+//   },
+
+//   loadMsgs: function() {
+//     $.ajax({
+//       url: app.server,
+//       data: { order: '-createdAt'},
+//       contentType: 'application/json',
+//       success: function(json) {
+//         app.displayMessages(json.results);
+//       },
+//       complete: function() {
+//         app.stopSpinner();
+//       }
+//     })
+//   },
+
+//   sendMsg: function(message) {
+//     app.startSpinner();
+//     $.ajax({
+//       type: 'POST',
+//       url: app.server,
+//       data: JSON.stringify(message),
+//       contentType: 'application/json',
+//       success: function(json) {
+//         message.objectId = json.objectId;
+//         app.displayMessage(message)
+//       },
+//       complete: function() {
+//         app.stopSpinner();
+//       }
+//     });
+//   },
+
+//   startSpinner: function() {
+//     $('spinner img').show();
+//     $('form input[type=submit]').attr('disabled', "true");
+//   },
+
+//   stopSpinner: function() {
+//     $('.spinner img').fadeOut('fast');
+//     $('form input[type=submit]').attr('disabled', null);
+//   }
+
+// };
+
 
 
 
